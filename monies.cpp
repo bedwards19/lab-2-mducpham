53866d71cb93010d2135ee63c34d672440ff8e22
Duc Pham <dpham@gemini.cs.hamilton.edu>
2018-09-03 08:27:42 -0400

    problem_fixed

--- monies.cpp
+++ monies.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    unsigned int dollars = 4294967298;
+    unsigned long dollars = 4294967298;
     cout << "I have " << dollars << " dollars in my wallet." << endl;
     return 0;
 }
